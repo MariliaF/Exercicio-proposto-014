f621f919c8776bb67f29dbd65af10a6391e38d59
MariliaF <marilia.lara94@outlook.com>
2021-08-13 22:55:50 -0300

    commit ex014

---
+++ ex016.js
@@ -0,0 +1,23 @@
+for(c=1; c<=6; c++){
+    console.log(c)
+}
+
+
+
+
+
+
+/*
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <=6)
+*/
+
+/*
+var c = 1
+while (c <=6){
+    console.log('Tudo bem?')
+    c++
+}*/
